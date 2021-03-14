981e0c67e312143e1c1382c1698140e53e7f3936
smahendrancs <smahendrancs@gmail.com>
2021-03-14 11:36:01 +0530

    this is first commit

---
+++ src/Add_Calc.java
@@ -0,0 +1,14 @@
+
+public class Add_Calc {
+	
+	public static void add()
+	{
+		int a = 2;
+		int b =3;
+		int c;
+		c= a+b;
+		
+		System.out.println(c);
+	}
+
+}
---
+++ src/Main_Calc.java
@@ -0,0 +1,10 @@
+
+public class Main_Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add_Calc.add();
+
+	}
+
+}

d71e17f6bcbc6102f797f1ef4b50bd8358f2a4a4
smahendrancs <smahendrancs@gmail.com>
2021-03-14 13:39:59 +0530

    140

--- src/Add_Calc.java
+++ src/Add_Calc.java
@@ -4,8 +4,8 @@ public class Add_Calc {
 	public static void add()
 	{
 //		This is my final code to be in master
-		int a = 120;
-		int b = 120;
+		int a = 140;
+		int b = 140;
 		int c;
 		c= a + b;
 		

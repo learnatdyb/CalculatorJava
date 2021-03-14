3f3bc2007a79fc0ffec41cac0639195202617f43
bhuvanak42 <bhuvanak42@yahoo.in>
2021-03-14 13:16:05 +0530

    90+90

--- src/Add_Calc.java
+++ src/Add_Calc.java
@@ -4,8 +4,8 @@ public class Add_Calc {
 	public static void add()
 	{
 //		This is my final code to be in master
-		int a = 80;
-		int b = 80;
+		int a = 90;
+		int b = 90;
 		int c;
 		c= a + b;
 		
